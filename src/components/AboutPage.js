bcff0f282227658f5ca0ea70ae70378ccbfcaa0d
yildiztugba <okra.yildiz@gmail.com>
2022-03-31 23:52:12 +0200

    elements with <>

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -3,10 +3,10 @@ import React from 'react';
 class AboutPage extends React.Component{
     render(){
         return 
-        <React.Fragment>
+        <>
             <h2>About</h2>
             <p>This app uses React</p>
-        </React.Fragment>
+        </>
     }
 }
 export default AboutPage;
