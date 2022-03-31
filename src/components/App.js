187b21968a6e634c3f0a3939383ff99a704f5521
yildiztugba <okra.yildiz@gmail.com>
2022-04-01 00:09:22 +0200

    header with nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
+import Header from './common/Header';
+
 
 function App (){
-    const route = window.location.pathname;
-    if(route === "/about") return <AboutPage />;
-    return <HomePage />;
+    function getPAge(){
+        const route = window.location.pathname;
+        if(route === "/about") return <AboutPage />;
+        return <HomePage />;
+    }
+
+    return (
+    <div className="container-fluid">
+        <Header />
+        {getPAge()}
+    </div>
+    );
 
 }
 
