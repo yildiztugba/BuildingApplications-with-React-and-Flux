13f2d1ff58f21791fade1abbf25dbbdda93bb123
yildiztugba <okra.yildiz@gmail.com>
2022-03-31 23:50:24 +0200

    add more element with div

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class AboutPage extends React.Component{
     render(){
-        return <h2>About</h2>
+        return 
+        <div>
+            <h2>About</h2>
+            <p>This app uses React</p>
+        </div>
     }
 }
 export default AboutPage;
