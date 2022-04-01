748b6bf1cde8534b0971138ce92b7f078bb634fa
yildiztugba <okra.yildiz@gmail.com>
2022-04-01 22:24:04 +0200

    textInput for errors

---
+++ src/components/common/TextInput.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function TextInput (props){
+    let wrapperClass = "form-group";
+    if (props.error && props.error.lenth > 0){
+        wrapperClass += "has-error" ;
+    }
+    return(
+    <div className={wrapperClass}>
+    <label htmlFor={props.id}> {props.label} </label>
+    <div className="field">
+        <input
+            id={props.id}
+            type="text"
+            onChange={props.onChange}
+            name={props.name}
+            className="form-control"
+            value={props.value}
+        />
+        </div> 
+        {props.error && <div className="alert alert-danger">{props.error}</div>}
+    </div>
+);
+}
+
+export default TextInput;
