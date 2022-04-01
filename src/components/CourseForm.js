6128731df7c7d76acce9c11245c3e2636e3857eb
yildiztugba <okra.yildiz@gmail.com>
2022-04-01 22:34:17 +0200

    reusable input

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,21 +1,16 @@
 import React from "react";
+import TextInput from './common/TextInput';
 
 function CourseForm(props) {
   return (
     <form>
-      <div className="form-group">
-        <label htmlFor="title">Title</label>
-        <div className="field">
-          <input
+          <TextInput
             id="title"
-            type="text"
+            label="Title"
             onChange={props.onChange}
             name="title"
-            className="form-control"
             value={props.course.title}
           />
-        </div>
-      </div>
 
       <div className="form-group">
         <label htmlFor="author">Author</label>
@@ -34,20 +29,14 @@ function CourseForm(props) {
         </div>
       </div>
 
-      <div className="form-group">
-        <label htmlFor="category">Category</label>
-        <div className="field">
-          <input
-            type="text"
+          <TextInput
             id="category"
+            label="Category"
             name="category"
-            className="form-control"
             onChange={props.onChange}
             value={props.course.category}
           />
-        </div>
-      </div>
-
+  
       <input type="submit" value="Save" className="btn btn-primary" />
     </form>
   );
