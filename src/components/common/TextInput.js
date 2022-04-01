77977182b5823c3abfcfcb9b36127d3b220284a2
yildiztugba <okra.yildiz@gmail.com>
2022-04-01 22:24:38 +0200

    propTypes for textinput

--- src/components/common/TextInput.js
+++ src/components/common/TextInput.js
@@ -24,4 +24,14 @@ function TextInput (props){
 );
 }
 
+
+TextInput.propTypes={
+    id:PropTypes.string.isRequired,
+    name:PropTypes.string.isRequired,
+    label:PropTypes.string.isRequired,
+    onChange:PropTypes.func.isRequired,
+    value: PropTypes.string,
+    error:PropTypes.string
+};
+
 export default TextInput;
