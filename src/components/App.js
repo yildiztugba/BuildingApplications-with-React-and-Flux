03f5099080730213ec5ad816c5f6eb50d2ca49ea
yildiztugba <okra.yildiz@gmail.com>
2022-04-01 11:48:02 +0200

    added route for notfoundPage with switch

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,20 @@ import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import Header from './common/Header';
 import CoursesPage from './CoursesPage';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
+import NotFoundPage from './NotFoundPage';
 
 function App (){
 
     return (
     <div className="container-fluid">
         <Header />
+        <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/courses" component={CoursesPage} />
         <Route path="/about" component={AboutPage} />
+        <Route component={NotFoundPage} />
+        </Switch>
     </div>
     );
 
