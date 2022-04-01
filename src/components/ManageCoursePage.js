c7b73b67b435facbb6d5d03433abd466df580465
yildiztugba <okra.yildiz@gmail.com>
2022-04-01 21:41:27 +0200

    useState + handleTitlechange + update value

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import { func } from 'assert-plus';
+import React,{useState} from 'react';
 import CourseForm from './CourseForm';
 
 const ManageCoursePage = props => {
+    const [course, setCourse] = useState ({
+        id:null,
+        slug:"",
+        title:"",
+        authorId:"",
+        category:""
+    });
+
+    function handleTitleChange(event){
+        const updateCourse ={...course,title: event.target.value};
+        setCourse(updateCourse);
+    }
 return (
     <>
     <h2>Manage Course</h2>
-    <CourseForm />
+    <CourseForm course={course}  onTitleChange={handleTitleChange}/>
     </>
 )
 }
