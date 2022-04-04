c0e1b10bc33e7491326ac0d136944087ccbc4a1e
yildiztugba <okra.yildiz@gmail.com>
2022-04-04 12:49:23 +0200

    changed haddleSubmit func with courseActions

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -3,6 +3,7 @@ import React,{useState,useEffect} from 'react';
 import CourseForm from './CourseForm';
 import courseStore from '../stores/courseStore';
 import {toast} from "react-toastify";
+import * as courseActions from "../actions/courseActions";
 
 const ManageCoursePage = props => {
 
@@ -45,7 +46,7 @@ const ManageCoursePage = props => {
         event.preventDefault();
 
         if(!formIsValid()) return;
-        courseApi.saveCourse(course).then(()=>{
+        courseActions.saveCourse(course).then(()=>{
             props.history.push("/courses");
             toast.success("Course saved");
         });
