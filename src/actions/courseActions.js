c887fd0d643a8d4325096265c455aab06a92389e
yildiztugba <okra.yildiz@gmail.com>
2022-04-04 11:14:31 +0200

    import actionTypes + added return

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,12 +1,15 @@
 import dispatcher from '../appDispatcher';
 import * as courseApi from '../api/courseApi';
+import actionTypes from './actionTypes';
+
 
 export function saveCourse(course){
 
-    courseApi.saveCourse(course).then(savedCourse => {
+    return courseApi.saveCourse(course).then(savedCourse => {
+        //hey dispatcher, go tell all the store that a course was just created
         dispatcher.dispatch({
-            actionType: "CREATE_COURSE",
+            actionType: actionTypes.CREATE_COURSE,
             course: savedCourse
-        })
-    })
+        });
+    });
 }
