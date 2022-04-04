7ec3d5ddd786eda7026f84a145b73996e9f2fe1b
yildiztugba <okra.yildiz@gmail.com>
2022-04-04 15:44:09 +0200

    store functions, store course with action

--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -1,7 +1,9 @@
 import {EventEmitter} from 'events';
 import Dispatcher from '../appDispatcher';
+import actionTypes from '../actions/actionTypes';
 
 const CHANGE_EVENT = "change";
+let _courses = [];
 class CourseStore extends EventEmitter{
     addChangeListener(callback){
         this.on(CHANGE_EVENT, callback);
@@ -14,12 +16,29 @@ class CourseStore extends EventEmitter{
     emitChange(){
         this.emit(CHANGE_EVENT);
     }
+
+    getCourses(){
+        return _courses;
+    }
+
+    getCourseBySLug(slug){
+        return _courses.find(course => course.slug === slug);
+    }
 }
+
+
+const store = new CourseStore ();
 
 Dispatcher.register(action=> {
     switch(action.actionType){
-        
+        case actionTypes.CREATE_COURSE:
+            _courses.push(action.course);
+            store.emitChange();
+            break;
+            default:
+                //nothing to do here
+
     }
 })
-const store = new CourseStore ();
+
 export default store;
