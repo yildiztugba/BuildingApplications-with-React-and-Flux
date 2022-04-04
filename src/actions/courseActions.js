810ebcc575653e31332698437e23d7985f215494
yildiztugba <okra.yildiz@gmail.com>
2022-04-04 16:18:38 +0200

    updated course data with flux

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,7 @@
 import dispatcher from '../appDispatcher';
 import * as courseApi from '../api/courseApi';
 import actionTypes from './actionTypes';
+import CourseForm from '../components/CourseForm';
 
 
 export function saveCourse(course){
@@ -8,7 +9,9 @@ export function saveCourse(course){
     return courseApi.saveCourse(course).then(savedCourse => {
         //hey dispatcher, go tell all the store that a course was just created
         dispatcher.dispatch({
-            actionType: actionTypes.CREATE_COURSE,
+            actionType: course.id 
+            ? actionTypes.UPDATE_COURSE
+            : actionTypes.CREATE_COURSE,
             course: savedCourse
         });
     });
@@ -24,3 +27,4 @@ export function loadCourses(){
         });
     });
 }
+
--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -40,6 +40,12 @@ Dispatcher.register(action=> {
             _courses = action.courses;
             store.emitChange();
             break;
+
+        case actionTypes.UPDATE_COURSE:
+            _courses = _courses.map( course => 
+                course.id === action.course.id ? action.course : course);
+                store.emitChange();
+                break;
             default:
                 //nothing to do here
 
